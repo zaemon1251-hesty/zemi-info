2b187f954521fc79a15abd3f4e756f6a9fe6fc1e
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-06-14 22:00:53 +0900

    assignment

---
+++ ch07/practice-1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from mypulp import Model, GRB, quicksum
+import numpy as np
+
+PRIMARY = True
+if len(sys.argv) >= 2:
+    print(sys.argv)
+    PRIMARY = bool(sys.argv[1] != "False")
+
+model = Model("lptest")
+
+b = [20, 10]
+
+A = [
+    [1, 1],
+    [3, 1],
+    [1, 2]
+]
+
+c = [6, 12, 10]
+
+if PRIMARY:
+    x = []
+    for i in range(2):
+        x.append(model.addVar(vtype="C", name=f"x{i}"))
+
+    for j in range(3):
+        model.addConstr(quicksum(xi * aij for xi, aij in zip(x, A[j])) <= c[j])
+
+    model.update()
+
+    model.setObjective(quicksum(bi * xi for bi, xi in zip(b, x)), GRB.MAXIMIZE)
+    model.optimize()
+
+    for xi in x:
+        print(f"{xi.name} = {xi.X}")
+    print(f"Optimal Val = {model.ObjVal}")
+
+else:
+    y = []
+    # 転置
+    At = np.asarray(A).T.tolist()
+    print(A)
+    for i in range(3):
+        y.append(model.addVar(vtype="C", name=f"y{i}"))
+
+    for j in range(2):
+        model.addConstr(
+            quicksum(
+                yi * atij for yi, atij in zip(y, At[j])
+            ) == b[j]
+        )
+
+    model.update()
+
+    model.setObjective(quicksum(ci * yi for ci, yi in zip(c, y)), GRB.MINIMIZE)
+    model.optimize()
+
+    for yi in y:
+        print(f"{yi.name} = {yi.X}")
+    print(f"Optimal Val = {model.ObjVal}")
